c8ec149235c53d2da8035da71f9fcaf1896de213
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-09-20 13:01:25 +0200

    Add new components for stylying

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { GetImageList } from '../actions/imageActions';
+import './ImageList.css';
 
 const ImageList = () => {
   const dispatch = useDispatch();
@@ -24,27 +24,33 @@ const ImageList = () => {
     if (!_.isEmpty(imageList.data)) {
       return (
         // imageList.data[0].date
-        imageList.data.map((el) => (
-          <p>{el.date}</p>
-
+        imageList.data.map((el, t) => (
+          <div>
+                <img
+          alt='image3'
+          src={el.url}
+        />
+          <h3 key={t}>{el.title}</h3>
+          <p key={t}>{el.date}</p>
+          <p key={t}>{el.explanation}</p>
+          </div>
         ))
-        
-        )
-
-
-     
-
-    
-    
+         )
     }
-
     if (imageList.loading) {
-      return <p>loading</p>;
+      return( <>
+    
+      <div className="loadingio-spinner-spinner-42jc9me6ch"><div className="ldio-ql2ovzsg3s9">
+<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+</div></div>
+<h3>loading...</h3>
+      </>
+      );
     }
     if (imageList.errorMessage !== '') {
       return <p>{imageList.errorMessage}</p>;
     }
-    return <p>Unable to get data</p>;
+    return <h3>ERROR IN GETTING DATA. PLEASE CONTACT SUPPORT</h3>;
   };
 
   return (
