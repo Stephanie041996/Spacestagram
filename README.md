78ffe57428dc73642ffc42cddaad0cb774be5729
Stephanie Michelle Sakuhuni <michellesakuhuni5@gmail.com>
2021-09-20 21:27:21 +0200

    Add readme files

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ A Space Image  app to view lots of cool images from NASA using the Nasa API  Ast
 
  Users can view images, the App adds the current days image each day to the data, so they are always up to date. Users are able to like and unlike thier favorite images by clicking the click button.
 
-![screenshot](./Screen-Shot.png)
+## Live Demo
+# [view site]()
+
+![screenshot](./Screen-shot.png)
 
 ## BUILT WITH
 - React
@@ -16,8 +19,6 @@ A Space Image  app to view lots of cool images from NASA using the Nasa API  Ast
 - Redux
 - CSS
 
-## Live Demo
-# [view site]()
 
 ## Getting Started
 # Prerequisities
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,50 +3,63 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { GetImageList } from '../actions/imageActions';
 import '../Style/ImageList.css';
-import ImageCard from './imageCard'
-import Nav from './nav'
+import ImageCard from './imageCard';
+import Nav from './nav';
 
 const ImageList = () => {
   const dispatch = useDispatch();
   const imageList = useSelector((state) => state.ImageList);
 
-  React.useEffect(() => {
-    FetchData();
-  }, []);
-
   const FetchData = () => {
     dispatch(GetImageList());
   };
 
+  React.useEffect(() => {
+    FetchData();
+  }, []);
 
   const ShowData = () => {
     if (!_.isEmpty(imageList.data)) {
       return (
         <div className="list-wrapper">
-        {imageList.data.map((el, t) => (
-          <div className="column" key={t.date}>
-         
-            <ImageCard 
-            title={el.title}
-            date={el.date}
-            url={el.url}
-            explanation={el.explanation}
-            id={t.date}
-          />
+          {imageList.data.map((el, t) => (
+            <div className="column" key={t.date}>
 
-       </div>
-        ))}
+              <ImageCard
+                title={el.title}
+                date={el.date}
+                url={el.url}
+                explanation={el.explanation}
+                id={t.date}
+              />
+
+            </div>
+          ))}
         </div>
-         )
+      );
     }
     if (imageList.loading) {
-      return( <>
-    
-      <div className="loadingio-spinner-spinner-42jc9me6ch"><div className="ldio-ql2ovzsg3s9">
-<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
-</div></div>
-<h3>loading...</h3>
-      </>
+      return (
+        <>
+
+          <div className="loadingio-spinner-spinner-42jc9me6ch">
+            <div className="ldio-ql2ovzsg3s9">
+              <div />
+              <div />
+              <div />
+              <div />
+              <div />
+              <div />
+              <div />
+              <div />
+              <div />
+              <div />
+              <div />
+              <div />
+            </div>
+          </div>
+          <h3>loading...</h3>
+        </>
       );
     }
     if (imageList.errorMessage !== '') {
